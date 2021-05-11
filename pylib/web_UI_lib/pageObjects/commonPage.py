388f0ff3e65c316fce34ebe796a79454039eded2
Hquanquan <1432047930@qq.com>
2021-05-11 23:47:50 +0800

    2021-5-11

---
+++ pylib/web_UI_lib/pageObjects/commonPage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/11 23:31
+# @Author  : 黄权权
+# @File    : commonPage.py
+# @Software: PyCharm
+# @Desc    : 淘宝-顶部导航栏
+from pylib.web_UI_lib.pageObjects.basePage import BasePage
+
+
+class CommonPage(BasePage):
+    """
+    淘宝-顶部导航栏
+    """
+
+    # 中国大陆
+    area_ele = ["css selector","#J_SiteNavBd .site-nav-menu-hd .site-nav-region"]
+
+
+
+
+    pass
